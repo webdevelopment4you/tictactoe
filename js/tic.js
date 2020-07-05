abe7627fc82a0aa528015616f672ce5d4b0ba79d
webdevelopment4you <65403221+webdevelopment4you@users.noreply.github.com>
2020-07-05 07:14:10 +0530

    Add files via upload

--- js/tic.js
+++ js/tic.js
@@ -26,7 +26,7 @@ function clicked(myId)
 {
     console.log(myId);
 
-    counter += 1;
+    
 
     console.log(counter);
 
@@ -38,6 +38,8 @@ function clicked(myId)
       <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
       </svg>`
 
+      counter += 1;
+
       winCheck();
     } 
 
@@ -47,6 +49,8 @@ function clicked(myId)
       <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
       <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
     </svg> `
+
+    counter += 1;
 
     winCheck();
   }
@@ -179,11 +183,6 @@ function winCheck()
 
 }
 
-function removeOnclick()
-{
-
-
-}
 
 
 
